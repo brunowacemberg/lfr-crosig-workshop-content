4714a61f0de1179f2fe1705dc6c8c6fd6c640a40
Bruno Wacemberg <brunowacemberg@Bruno-Wacembergs-Macbook-Pro.local>
2023-05-28 16:13:41 -0300

    First commit

---
+++ modules/contacts/contacts-service/src/main/resources/META-INF/sql/indexes.sql
@@ -0,0 +1,3 @@
+create index IX_F949FA0A on CROSIG_Foo (field2);
+create index IX_28C98A92 on CROSIG_Foo (uuid_[$COLUMN_LENGTH:75$], companyId);
+create unique index IX_1F78D94 on CROSIG_Foo (uuid_[$COLUMN_LENGTH:75$], groupId);
